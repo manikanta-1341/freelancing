a90601ee1a4a29ea491ae7bb953bcfc190cae745
manikanta1341 <medicharlamanikanta13@gmail.com>
2022-08-27 16:12:07 +0530

    first commit

--- client/src/url/url.js
+++ client/src/url/url.js
@@ -1,1 +1,2 @@
-export const backend_url = 'http://localhost:5000'
+export const backend_url = 'https://domaincer-freelancer-api.herokuapp.com'
+// export const backend_url = 'http://localhost:5000'
